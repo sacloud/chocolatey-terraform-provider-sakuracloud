ff6e5dbb6bf352687a95d75c097f29ebe57bb1e5
223n <223n@223n.tech>
2026-04-08 12:32:44 +0900

    mod: 32bit/64bit関係なくアンインストール処理を呼び出すように修正
    Signed-off-by: 223n <223n@223n.tech>

--- terraform-provider-sakuracloud/tools/chocolateyuninstall.ps1
+++ terraform-provider-sakuracloud/tools/chocolateyuninstall.ps1
@@ -1,15 +1,6 @@
 ﻿$ErrorActionPreference = 'Stop';
 
-$packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = 'terraform-provider-sakuracloud*'
-  zipFileName   = 'terraform-provider-sakuracloud_windows_386.zip'
-  zipFileName64 = 'terraform-provider-sakuracloud_windows_amd64.zip'
-}
+$packageName = $env:ChocolateyPackageName;
 
-$os = Get-WmiObject -Class Win32_OperatingSystem;
-if ($os.OSarchitecture.Contains("64")) {
-  Uninstall-ChocolateyZipPackage -PackageName $packageArgs['packageName'] -ZipFileName $packageArgs['zipFileName64']
-} else {
-  Uninstall-ChocolateyZipPackage -PackageName $packageArgs['packageName'] -ZipFileName $packageArgs['zipFileName']
-}
+Uninstall-ChocolateyZipPackage -PackageName $packageName -ZipFileName 'terraform-provider-sakuracloud___VERSION___windows_386.zip'
+Uninstall-ChocolateyZipPackage -PackageName $packageName -ZipFileName 'terraform-provider-sakuracloud___VERSION___windows_amd64.zip'
